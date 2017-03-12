6f503c7130105f6058584b02fd9647f58557f652
OneRandomMikey <zehua.li6@gmail.com>
2017-03-12 19:32:01 -0400

    adding hovered over feature

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-document.getElementById('click1').addEventListener('click', function(){
+document.getElementById('click1').addEventListener('click', function () {
   var audio = new Audio('drum-sounds/boom.wav')
   audio.play()
 })
 
-document.getElementById('click2').addEventListener('click', function(){
+document.getElementById('click2').addEventListener('click', function () {
   var audio = new Audio('drum-sounds/clap.wav')
   audio.play()
 })
 
-document.getElementById('click3').addEventListener('click', function(){
+document.getElementById('click3').addEventListener('click', function () {
+  var audio = new Audio('drum-sounds/ride.wav')
+  audio.play()
+})
+
+document.getElementById('click1').addEventListener('mouseenter',function(){
+  var audio = new Audio('drum-sounds/boom.wav')
+  audio.play()
+})
+
+document.getElementById('click2').addEventListener('mouseenter', function () {
+  var audio = new Audio('drum-sounds/clap.wav')
+  audio.play()
+})
+
+document.getElementById('click3').addEventListener('mouseenter', function () {
   var audio = new Audio('drum-sounds/ride.wav')
   audio.play()
 })
